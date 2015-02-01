1ea28d0af285cedae2beb64abbd48768e78c2d03
Robert <robert.apb@hotmail.com>
2015-02-01 00:51:03 -0400

    Creating struct interfaces

---
+++ printhouse/structs.go
@@ -0,0 +1,17 @@
+// Structs to be used on this library,
+// contains analog models to the ones
+// on api server side.
+package printhouse
+
+type Product struct{
+
+}
+type Order struct{
+
+}
+type ShippingQuote struct{
+
+}
+type PrintFile struct{
+
+}
