66c7efc85f189f7f69d5a0fcfd27055fd598d722
Robert <robert.apb@hotmail.com>
2015-02-01 00:55:31 -0400

    Adding structs attrs...

--- printhouse/structs.go
+++ printhouse/structs.go
@@ -4,14 +4,26 @@
 package printhouse
 
 type Product struct{
-
+	id          string
+	kind        string
+	description string
+	price       float64
+	x_inches    int16
+	y_inches    int16
+	x_pixels    int16
+	y_pixels    int16
+	format      string
 }
+
+type PrintFile struct{
+	product  string
+	file_url string
+	id       string
+}
+
 type Order struct{
 
 }
 type ShippingQuote struct{
-
-}
-type PrintFile struct{
 
 }
