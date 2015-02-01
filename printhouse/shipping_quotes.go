63fcfd04a6788eac0cf9c7e0d81f2e903a085b6f
Robert <robert.apb@hotmail.com>
2015-02-01 01:13:55 -0400

    Added Shipping Quote interface

--- printhouse/shipping_quotes.go
+++ printhouse/shipping_quotes.go
@@ -3,3 +3,30 @@
 // shipping quotes negotiations with the server.
 package printhouse
 
+// Generates a shipping quote based on the information submitted
+//
+// If positive, the ShippingQuote will have a new Id.
+// Usage:
+//	1: ph.PostShippingQuote(&ShippingQuote{Product: "0001", FileUrl: "http://dom.myFile.host"})
+func (ph *Printhouse) PostShippingQuote(printfile *ShippingQuote) *ShippingQuote {
+	return nil
+}
+
+// Lists all shipping quotes for the user
+//
+// Usage:
+//	1: ph.GetShippingQuotes()
+func (ph *Printhouse) GetShippingQuotes() []ShippingQuote {
+	return nil
+}
+
+// Get's the specified shipping quote for the user
+//
+// Usage:
+//	1: ph.GetShippingQuote("54808653b7017c7f795a6ac6")
+func (ph *Printhouse) GetShippingQuote(id string) ShippingQuote {
+	return nil
+}
+
+
+
